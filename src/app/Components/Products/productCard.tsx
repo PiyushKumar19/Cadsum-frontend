3614a19908b0d13fa7982198bd2d1bc9ea165ecf
PiyushKumar19 <developerpiyush1102k2@gmail.com>
2024-08-23 14:49:14 +0530

    Fixed the aligment of Related Products in ProductOverview

--- src/app/Components/Products/productCard.tsx
+++ src/app/Components/Products/productCard.tsx
@@ -8,8 +8,8 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({ name, imageSrc }) => {
   return (
     <div>
-      <div className="relative m-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
-        <a className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="#">
+      <div className="relative m-2 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
+        <a className="relative mx-3 mt-1 flex h-60 overflow-hidden rounded-xl" href="#">
           <img className="object-cover" src={imageSrc} alt="product image" />
           {/* <span className="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">39% OFF</span> */}
         </a>
